f5b5391306a7ef136a5923f7731e141ace4053eb
Jorge Oliver <joliverpalma@gmail.com>
2017-05-29 19:24:47 -0400

    Repaso JS 2

---
+++ main.js
@@ -0,0 +1,20 @@
+//cciclo for
+//for(var i; i<10; i++){
+    //codigo acá
+//}
+
+//var users = ['Juan', 'Felipe', 'Rodrigo', 'Richard', 'Sebastian']
+//cciclo while
+//var  i = 0
+//while (i < users.length) {
+    //codigo amigo aquí
+  //  console.log(users[i])
+  //  i++
+//}
+
+// JQUERY
+// JQuery signo $() identifica la llamada a JQuery
+$(function()){
+// buscar etiquta por ID se llama con el elemento #  
+    console.log('#p')
+}
